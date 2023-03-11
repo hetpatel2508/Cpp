e7dccec9a8aadecfc9ea8c74dff77fb74fd6186c
hetpatel2508 <125752460+hetpatel2508@users.noreply.github.com>
2023-03-11 13:45:53 +0530

    vov

---
+++ constantvariable.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   const int a=2;
+    int b=3;
+    // a++;   const 6 atle e variable ne value kyarey change nai tay
+    b++;
+    cout<<a<<"   "<<b;
+}
