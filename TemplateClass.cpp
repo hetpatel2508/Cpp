22de03febd32f90bd0ae9a5ee37872965a63ad50
hetpatel2508 <125752460+hetpatel2508@users.noreply.github.com>
2023-03-11 10:50:52 +0530

    vikjvji

---
+++ TemplateClass.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+template<class t>
+class ract 
+{
+private:
+t len;
+t bre;
+
+public:
+
+ract(t l,t b)
+{
+    len=l;
+    bre=b;
+}
+
+t area();
+};
+
+template<class t>
+t ract<t>::area()
+{
+    return len*bre;
+}
+
+int main()
+{
+ract<float> r1(2.25,4.75);
+cout<<r1.area();
+}
