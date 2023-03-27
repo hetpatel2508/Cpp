ab4bf485bb56523eeac69d1df33042a214fc49be
hetpatel2508 <125752460+hetpatel2508@users.noreply.github.com>
2023-03-27 11:19:07 +0530

    visvsuv

---
+++ MacrosAndPreprocessorAll.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+#define pi 3.14
+#define sqr(x) (x*x)
+#define o cout
+#define i cin
+#define msg(x) #x
+#ifndef pi
+    #define pi 3
+    #endif
+
+
+int main()
+{
+    int n;
+o<<"Enter number: ";
+i>>n;
+
+o<<"Sqare= "<<sqr(n)<<endl;
+
+o<<"Massage= "<<msg(Hello! This is Het Patel)<<endl;
+
+o<<"Value of PI= "<<pi;
+
+}
