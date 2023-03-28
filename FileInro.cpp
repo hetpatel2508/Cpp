6bd1b1996c60ea0a1cc08f90f05fc4c22c223c89
hetpatel2508 <125752460+hetpatel2508@users.noreply.github.com>
2023-03-28 20:28:20 +0530

    nyyrujtn

--- FileInro.cpp
+++ FileInro.cpp
@@ -5,10 +5,22 @@ using namespace std;
 
 int main()
 {
+    string t;
     ofstream out("testfile.txt");
     out<<"Name: Het Patel"<<endl;
     out<<"E.number= 22BECE30273"<<endl;
     out<<"P.Num= 9265907918"<<endl;
 
     out.close();
+
+    ifstream in("testfile.txt");
+    getline(in,t);  //same as cin>>t
+    cout<<t<<endl;
+    getline(in,t);  //same as cin>>t
+    cout<<t<<endl;
+    getline(in,t);  //same as cin>>t
+    cout<<t;
+
+    in.close();
+
 }
