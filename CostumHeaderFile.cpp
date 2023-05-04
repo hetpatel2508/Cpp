1aa03b79cb52ccfb856a68bee62090edd8be9da7
hetpatel2508 <125752460+hetpatel2508@users.noreply.github.com>
2023-05-04 12:50:22 +0530

    kdjvna

---
+++ CostumHeaderFile.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include "Ctest.cpp"
+
+using namespace std;
+
+
+int main()
+{
+cout<<add(2,3)<<endl;
+cout<<multt(2,3)<<endl;
+cout<<divv(2,3)<<endl;
+}
---
+++ Ctest.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int add(int a,int b)
+{
+    return a+b;
+}
+
+int multt(int a,int b)
+{
+    return a*b;
+}
+
+int divv(int a,int b)
+{
+    return a/b;
+}
